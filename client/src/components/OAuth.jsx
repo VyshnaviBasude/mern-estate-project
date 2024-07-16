bc99d9dd36225815543d8316a9f4f6bc2a90ddc0
Vyshnavi <vyshnavibasude@gmail.com>
2024-07-15 22:53:51 -0400

    Update Header and make a profile page  private

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -8,8 +8,7 @@ import { useNavigate } from 'react-router-dom';
 export default function OAuth() {
     const dispatch = useDispatch();
         const navigate = useNavigate();
-    const handleGoogleClick=async() =>{
-        
+    const handleGoogleClick=async() =>{   
         try{
             const provider = new GoogleAuthProvider()
             const auth =getAuth(app)
