11732b46654dee3e68f4f34b33b2b351f6113eb1
Vyshnavi <vyshnavibasude@gmail.com>
2024-11-28 17:55:03 -0500

    Listing Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { app } from "../firebase";
 import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
 import { useRef, useState, useEffect } from "react";
 import {
   getStorage,
@@ -175,6 +176,9 @@ const handleSignOut = async() => {
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {loading? 'Loading...':'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+        Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDelete} className="text-red-300 cursor-pointer"> Delete Account</span>
