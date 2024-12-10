3be967357f120b9b836e473bd9f8dbb517f8818b
Vyshnavi <vyshnavibasude@gmail.com>
2024-12-10 16:44:57 -0500

    Update Listing API route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -3,7 +3,7 @@ import { eroorhandler } from '../utils/error.js';
 export const createListing = async(req, res, next) =>{
     try{
           const listing = await Listing.create(req.body);
-          return res.status(201).json(listing);
+          return res.status(200).json(listing);
     }catch(error)
     {
         next(error);
@@ -22,7 +22,26 @@ export const deleteListing = async(req, res, next) =>{
     }
     try{
           await Listing.findByIdAndDelete(req.params.id);
-          return res.status(201).json('Listing has been deleted');
+          return res.status(200).json('Listing has been deleted');
+    }catch(error)
+    {
+        next(error);
+    }
+};
+
+export const updateListing = async(req, res, next) =>{
+    const listing = await Listing.findById(req.params.id);
+    if(!listing)
+    {
+        return next(eroorhandler(404, 'Listing not found'));
+    }
+    if(req.user.id !== listing.userRef)
+    {
+        return next(eroorhandler(401, 'You can only Update your own listings!'));
+    }
+    try{
+          const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new:true});
+          return res.status(200).json(updatedListing);
     }catch(error)
     {
         next(error);
