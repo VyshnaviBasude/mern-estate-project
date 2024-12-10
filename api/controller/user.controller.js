a07a8f622f1029f0c3dd90df6e2fb11b6832f24d
Vyshnavi <vyshnavibasude@gmail.com>
2024-12-10 12:48:00 -0500

    User listings API route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { eroorhandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
+import Listing from "../models/listing.model.js";
 export const test = (req,res)=>{
     res.json({
         message:'API ROUTE !',
@@ -48,3 +49,18 @@ export const deleteUser =async (req,res,next)=>{
    }
 
 };
+
+export const getUserListings = async(req, res, next)=>{
+    if(req.user.id === req.params.id)
+    {
+      try{
+        const listings = await Listing.find({userRef:req.params.id});
+        res.status(200).json(listings);
+      }catch(error)
+      {
+           next(error)
+      }
+    }else{
+        return next(eroorhandler(401, 'You can only view your own listings'));
+    }
+}
